ec6e2896fe2255cea37e044b6c5a5087ae29eb78
Joseph Chu <joseph.chu@addepar.com>
2016-01-13 11:31:50 -0800

    update routes and update router map

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,15 +7,19 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('license');
-  this.route('overview');
-  this.route('documentation');
-  this.route('accordion');
-  this.route('carousel');
-  this.route('modal');
-  this.route('popover');
-  this.route('select');
-  this.route('colorPicker');
-  this.route('radioButton');
+  this.route('emberWidgets', {
+    path: '/ember-widgets'
+  }, function() {
+    this.route('overview');
+    this.route('documentation');
+    this.route('accordion');
+    this.route('carousel');
+    this.route('modal');
+    this.route('popover');
+    this.route('select');
+    this.route('colorPicker');
+    this.route('radioButton');
+  });
 });
 
 export default Router;
